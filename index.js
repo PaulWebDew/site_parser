e689c9734b80ff8c7eff84766535b44190803434
PaulWebDew <paulwebdew@gmail.com>
2023-06-06 12:43:27 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+import axios from 'axios';
+import fs from 'fs';
+import jsdom from 'jsdom';
+import { JSDOM } from 'jsdom';
+import express from 'express';
+import cors from 'cors';
+import puppeteer from 'puppeteer';
+
+const FILE = 'deserty.json';
+const LINK = 'https://sushistore.ru/menu/deserty/';
+(async () => {
+  try {
+    let browser = await puppeteer.launch({ headless: false, devtools: true });
+    let page = await browser.newPage();
+    await page.goto(LINK, { timeout: 0 });
+    await page.waitForSelector('.product-card__info.product-info');
+    let html = await page.evaluate(
+      async () => {
+        let result = [];
+        try {
+          let divs = document.querySelectorAll('.product-card ');
+          divs.forEach((el) => {
+            let title = el.querySelector('.product-card__title-link.link-inherit').innerText;
+            let price = el.querySelector('.product-price__value').innerText;
+            let info = el.querySelector('.product-info__portion').innerText;
+            let img = el.querySelector('.product-card__img').src;
+            let obj = {
+              title: title,
+              price: price,
+              info: info,
+              img: img,
+            };
+            result.push(obj);
+          });
+        } catch (err) {
+          console.log('Error evaluate:' + err);
+        }
+
+        return result;
+      },
+      {
+        waitUntill: '.product-card__info product-info',
+      },
+    );
+    fs.writeFileSync(FILE, JSON.stringify(html), (err) => {
+      console.log('write file ERROR:' + err);
+    });
+    await browser.close();
+  } catch (err) {
+    await browser.close();
+    console.log(err);
+  }
+})();
